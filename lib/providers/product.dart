c498bcb9cf4402ff396421ad1a52e0f9b27c09d8
Mohamed Magdy <mo.magdy174@gmail.com>
2020-02-15 18:32:59 +0200

    API Toggle Favorite Status

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,5 +1,8 @@
-import 'package:flutter/foundation.dart';
+import 'dart:convert';
 
+import 'package:flutter/foundation.dart';
+import '../models/url_api.dart';
+import 'package:http/http.dart' as http;
 class Product with ChangeNotifier {
   final String id;
   final String title;
@@ -17,9 +20,35 @@ class Product with ChangeNotifier {
     this.isFavorite = false,
   });
 
-  void toggleFavoriteStatus() {
-    isFavorite = !isFavorite;
+  void _setFavValue(bool newValue){
+    isFavorite = newValue;
     notifyListeners();
   }
+
+  Future<void> toggleFavoriteStatus() async{
+
+    final oldStatus = isFavorite;
+    isFavorite = !isFavorite;
+    notifyListeners();
+
+    final url = '$api/products/$id.json';
+    try{
+      final response =  await http.patch(url, body: json.encode({
+        'isFavorite': isFavorite,
+      },),);
+      if(response.statusCode >= 400){
+        _setFavValue(oldStatus);
+      }
+    }catch(error){
+      _setFavValue(oldStatus);
+    }
+
+
+
+
+
+  }
+
+
 
 }
